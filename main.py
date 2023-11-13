95552196002c2786bac0fd4f8635283f724b1c40
pf4 <74263128+elydre@users.noreply.github.com>
2023-11-13 21:33:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ SWITCH_COLOR_OFF = (99, 129, 135)
 SWITCH_COLOR_IO = (230, 235, 237)
 SWITCH_COLOR_BAR = (0, 0, 0)
 
+MARBLE_SPEED = 5
+
 X_OFFSET = 0
 Y_OFFSET = 0
 ZOOM = 1
@@ -25,9 +27,13 @@ def get_x(x):
 def get_y(y):
     return round((y - Y_OFFSET) * ZOOM)
 
+def gen_color(x, y):
+    return (x * 50 % 180, y * 50 % 180, (x + y) * 50 % 180)
+
 class IO:
-    def __init__(self, switch, index):
-        self.switch = switch
+    def __init__(self, ptd_sw, index, current = None):
+        self.ptd_sw = ptd_sw
+        self.current_sw = current
         self.index = index
 
 class Switch:
@@ -121,17 +127,66 @@ class Switch:
         # draw io
         for i in range(3):
             if self.io[i] is not None:
-                pygame.draw.line(screen, SWITCH_COLOR_IO, self.get_io_pos(i), self.io[i].switch.get_io_pos(self.io[i].index), round(2 * ZOOM))
+                x = self.get_io_pos(i)
+                y = x[1]
+                x = x[0]
+                pygame.draw.line(screen, gen_color(x, y), self.get_io_pos(i), self.io[i].ptd_sw.get_io_pos(self.io[i].index), round(2 * ZOOM))
 
     def toggle(self):
         self.active = not self.active
 
     def rotate(self):
         self.rotation = (self.rotation + 1) % 4
+    
+    def get_directed_out(self):
+        if self.active:
+            return self.io[2]
+        else:
+            return self.io[1]
+
+class Marble:
+    def __init__(self, color):
+        self.color = color
+        self.into = [None, None]
+        self.traveled = 0
+        self.length = 0
+        self.name = "THE MARBLE"
+
+    def draw(self):
+        if self.into[0] is None:
+            return
+        x1 = self.into[0].ptd_sw.get_io_pos(self.into[0].index)[0]
+        y1 = self.into[0].ptd_sw.get_io_pos(self.into[0].index)[1]
+        if self.into[1] is None:
+            pygame.draw.circle(screen, self.color, (x1, y1), round(5 * ZOOM))
+            return
+        x2 = self.into[1].ptd_sw.get_io_pos(self.into[1].index)[0]
+        y2 = self.into[1].ptd_sw.get_io_pos(self.into[1].index)[1]
+        x = x1 + (x2 - x1) * self.traveled / self.length
+        y = y1 + (y2 - y1) * self.traveled / self.length
+        pygame.draw.circle(screen, self.color, (x, y), round(5 * ZOOM))
+
+    def move(self):
+        if self.into[0] is None or self.into[1] is None:
+            return
+        if self.traveled == 0:
+            self.length = ((get_x(self.into[0].ptd_sw.get_io_pos(self.into[0].index)[0]) - get_x(self.into[1].ptd_sw.get_io_pos(self.into[1].index)[0])) ** 2 + (get_y(self.into[0].ptd_sw.get_io_pos(self.into[0].index)[1]) - get_y(self.into[1].ptd_sw.get_io_pos(self.into[1].index)[1])) ** 2) ** 0.5
+        self.traveled += MARBLE_SPEED
+        if self.traveled > self.length:
+            self.into[0] = self.into[1].ptd_sw.io[2 if self.into[1].ptd_sw.active else 1]
+            self.into[1] = self.into[0].ptd_sw.io[self.into[0].index]
+            (self.into[0], self.into[1]) = (self.into[1], self.into[0])
+            if self.into[1] is None:
+                print("MARBLE BAD SWITCH")
+            print("MARBLE SWITCH")
+            self.traveled = 0
+            self.length = ((m.into[1].ptd_sw.x - m.into[0].ptd_sw.x) ** 2 + (m.into[1].ptd_sw.y - m.into[0].ptd_sw.y) ** 2) ** 0.5
 
 mouse_down = [-1, -1]
 
 s = [Switch(0, 0, "init")]
+m = Marble((255, 0, 0))
+
 tmp_io = None
 
 while True:
@@ -165,24 +220,24 @@ while True:
                         found = True
                         if event.button == 3: # delete io
                             if crsw.io[i] is not None:
-                                crsw.io[i].switch.io[crsw.io[i].index] = None
+                                crsw.io[i].ptd_sw.io[crsw.io[i].index] = None
                                 crsw.io[i] = None
                         else:
                             if tmp_io is None:
                                 tmp_io = IO(crsw, i)
                             else:
-                                if tmp_io.switch is not crsw:
-                                    if tmp_io.switch.io[tmp_io.index] is not None:
+                                if tmp_io.ptd_sw is not crsw:
+                                    if tmp_io.ptd_sw.io[tmp_io.index] is not None:
                                         print("delete old io")
-                                        tmp_io.switch.io[tmp_io.index].switch.io[tmp_io.switch.io[tmp_io.index].index] = None
+                                        tmp_io.ptd_sw.io[tmp_io.index].ptd_sw.io[tmp_io.ptd_sw.io[tmp_io.index].index] = None
                                     if crsw.io[i] is not None:
                                         print("delete new io")
-                                        crsw.io[i].switch.io[crsw.io[i].index] = None
+                                        crsw.io[i].ptd_sw.io[crsw.io[i].index] = None
 
-                                    tmp_io.switch.io[tmp_io.index] = IO(crsw, i)
+                                    tmp_io.ptd_sw.io[tmp_io.index] = IO(crsw, i)
                                     crsw.io[i] = tmp_io
-                                    print(tmp_io.switch.io[tmp_io.index].switch.name)
-                                    print(crsw.io[i].switch.name)
+                                    print(tmp_io.ptd_sw.io[tmp_io.index].ptd_sw.name)
+                                    print(crsw.io[i].ptd_sw.name)
                                     tmp_io = None
                                 else:
                                     print("same switch")
@@ -195,7 +250,7 @@ while True:
                         if event.button == 3:
                             for i in range(3):
                                 if crsw.io[i] is not None:
-                                    crsw.io[i].switch.io[crsw.io[i].index] = None
+                                    crsw.io[i].ptd_sw.io[crsw.io[i].index] = None
                             s.remove(crsw)
                         else:
                             crsw.toggle()
@@ -222,25 +277,51 @@ while True:
                 X_OFFSET = 0
                 Y_OFFSET = 0
                 ZOOM = 1
-            if event.key == pygame.K_l:
+            elif event.key == pygame.K_l:
                 # list all switches and their io
                 print("switches:")
                 for crsw in s:
                     print(crsw.name)
                     for i in range(3):
                         if crsw.io[i] is not None:
-                            print(f"  {i}: {crsw.io[i].switch.name}")
+                            print(f"  {i}: {crsw.io[i].ptd_sw.name}")
                         else:
                             print(f"  {i}: None")
-            if event.key == pygame.K_r:
+                # list all marbles
+                print("marble:")
+                print(f"  into[0]: {m.into[0].ptd_sw.name if m.into[0] is not None else None}")
+                print(f"  into[1]: {m.into[1].ptd_sw.name if m.into[1] is not None else None}")
+                print(f"  traveled: {m.traveled}")
+                print(f"  length: {m.length}")
+
+
+            elif event.key == pygame.K_r:
                 for crsw in s:
                     if get_x(crsw.x) < pygame.mouse.get_pos()[0] < get_x(crsw.x) + 20 * ZOOM and get_y(crsw.y) < pygame.mouse.get_pos()[1] < get_y(crsw.y) + 20 * ZOOM:
                         crsw.rotate()
+                        break
+            elif event.key == pygame.K_m:
+                for crsw in s:
+                    if get_x(crsw.x) < pygame.mouse.get_pos()[0] < get_x(crsw.x) + 20 * ZOOM and get_y(crsw.y) < pygame.mouse.get_pos()[1] < get_y(crsw.y) + 20 * ZOOM:
+                        print(f"set marble to {crsw.name}")
+                        m.into[1] = crsw.get_directed_out()
+                        if m.into[1] is not None:
+                            m.into[0] = m.into[1].ptd_sw.io[m.into[1].index]
+                            m.length = ((m.into[1].ptd_sw.x - m.into[0].ptd_sw.x) ** 2 + (m.into[1].ptd_sw.y - m.into[0].ptd_sw.y) ** 2) ** 0.5
+                        else:
+                            m.into[0] = None
+                            m.length = 0
+                        m.traveled = 0
                         break
 
     screen.fill((0, 0, 0))
     for switch in s:
         switch.draw()
+    
+    # move marble
+    m.move()
+    m.draw()
+
 
     # draw fps
     fps = f"fps: {int(clock.get_fps())} [{X_OFFSET}, {Y_OFFSET}] x{ZOOM}"
